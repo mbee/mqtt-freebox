7098276fa295272f73a38106bfc6e93d36b770a7
Marc Bee <marc.bee@gmail.com>
2019-12-19 21:55:49 +0100

    simplifying includes

--- mqtt-freebox.go
+++ mqtt-freebox.go
@@ -4,9 +4,6 @@ import (
 	"bytes"
 	"encoding/json"
 	"fmt"
-	"io/ioutil"
-	"net/http"
-	"net/url"
 	"os"
 	"os/signal"
 	"strings"
@@ -21,69 +18,51 @@ var fbx *freebox.Client
 var cli *client.Client
 var sigc chan os.Signal
 
-var SMS_LOGIN, SMS_PASS, MQTT_URL, MQTT_LOGIN, MQTT_PASSWORD string
+var (
+	MQTT_URL      string
+	MQTT_LOGIN    string
+	MQTT_PASSWORD string
+)
 
-func main() {
-	// Set up channel on which to send signal notifications.
-	sigc = make(chan os.Signal, 1)
-	signal.Notify(sigc, os.Interrupt, os.Kill)
-	if os.Getenv("DEBUG") != "" {
-		log.SetLevel(log.DebugLevel)
-	}
-	SMS_LOGIN = os.Getenv("GOFBX_SMS_LOGIN")
-	SMS_PASS = os.Getenv("GOFBX_SMS_PASS")
-	MQTT_URL = os.Getenv("GOFBX_MQTT_URL")
-	MQTT_LOGIN = os.Getenv("GOFBX_MQTT_LOGIN")
-	MQTT_PASSWORD = os.Getenv("GOFBX_MQTT_PASSWORD")
+// LanHost
+type LanHost struct {
+	L2Ident struct {
+		ID   string `json:"id"`
+		Type string `json:"type"`
+	} `json:"l2ident"`
+	Active            bool          `json:"active"`
+	ID                string        `json:"id"`
+	LastTimeReachable freebox.Epoch `json:"last_time_reachable"`
+	Persistent        bool          `json:"persistent"`
+	VendorName        string        `json:"vendor_name"`
+	HostType          string        `json:"host_type"`
+	PrimaryName       string        `json:"primary_name"`
+	L3Connectivities  []struct {
+		Addr              string        `json:"addr"`
+		Active            bool          `json:"active"`
+		Reachable         bool          `json:"reachable"`
+		LastActivity      freebox.Epoch `json:"last_activity"`
+		AF                string        `json:"af"`
+		LastTimeReachable freebox.Epoch `json:"last_time_reachable"`
+	} `json:"l3connectivities"`
+	Reachable         bool          `json:"reachable"`
+	LastActivity      freebox.Epoch `json:"last_activity"`
+	PrimaryNameManual bool          `json:"primary_name_manual"`
+	Interface         string        `json:"interface"`
+}
 
-	initMqtt()
-	log.Info("Mqtt ... OK")
-	defer cli.Terminate()
+// Get all lan hosts
+func GetLanHosts(c *freebox.Client) ([]LanHost, error) {
+	payload := []LanHost{}
+	err := c.GetResult("lan/browser/pub/", &payload)
+	return payload, err
+}
 
-	initFreebox()
-
-	// Subscribe to topics.
-	err := cli.Subscribe(&client.SubscribeOptions{
-		SubReqs: []*client.SubReq{
-			&client.SubReq{
-				TopicFilter: []byte("mqtt-freebox/get/host/#"),
-				QoS:         mqtt.QoS0,
-				// Define the processing of the message handler.
-				Handler: func(topicName, message []byte) {
-					device_id := strings.Split(string(topicName), "/")[3]
-
-					log.Info("get info for device: " + device_id)
-					lanhost, err := fbx.GetLanHost(device_id)
-					if err != nil {
-						log.Error(err)
-						return
-					}
-
-					log.Info("publishing mqtt-freebox/status/host/" + lanhost.ID)
-					payload := new(bytes.Buffer)
-					encoder := json.NewEncoder(payload)
-					if err := encoder.Encode(lanhost); err != nil {
-						log.Error(err)
-						return
-					}
-					payloadString := strings.TrimSpace(fmt.Sprintf("%s", payload))
-					publish("mqtt-freebox/status/host/"+lanhost.ID, payloadString)
-
-				},
-			},
-		},
-	})
-
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	<-sigc
-
-	// Disconnect the Network Connection.
-	if err := cli.Disconnect(); err != nil {
-		panic(err)
-	}
+// Get a contact
+func GetLanHost(c *freebox.Client, name string) (LanHost, error) {
+	payload := LanHost{}
+	err := c.GetResult(fmt.Sprintf("lan/browser/pub/%s", name), &payload)
+	return payload, err
 }
 
 func initMqtt() {
@@ -141,44 +120,63 @@ func initFreebox() {
 	}
 }
 
-// check if there's incomming call
-func checkCall() (*freebox.CallEntry, bool) {
-	calls, err := fbx.GetCallEntries()
+func main() {
+	// Set up channel on which to send signal notifications.
+	sigc = make(chan os.Signal, 1)
+	signal.Notify(sigc, os.Interrupt, os.Kill)
+	if os.Getenv("DEBUG") != "" {
+		log.SetLevel(log.DebugLevel)
+	}
+	MQTT_URL = os.Getenv("GOFBX_MQTT_URL")
+	MQTT_LOGIN = os.Getenv("GOFBX_MQTT_LOGIN")
+	MQTT_PASSWORD = os.Getenv("GOFBX_MQTT_PASSWORD")
+
+	initMqtt()
+	log.Info("Mqtt ... OK")
+	defer cli.Terminate()
+
+	initFreebox()
+
+	// Subscribe to topics.
+	err := cli.Subscribe(&client.SubscribeOptions{
+		SubReqs: []*client.SubReq{
+			&client.SubReq{
+				TopicFilter: []byte("mqtt-freebox/get/host/#"),
+				QoS:         mqtt.QoS0,
+				// Define the processing of the message handler.
+				Handler: func(topicName, message []byte) {
+					device_id := strings.Split(string(topicName), "/")[3]
+
+					log.Info("get info for device: " + device_id)
+					lanhost, err := GetLanHost(fbx, device_id)
+					if err != nil {
+						log.Error(err)
+						return
+					}
+
+					log.Info("publishing mqtt-freebox/status/host/" + lanhost.ID)
+					payload := new(bytes.Buffer)
+					encoder := json.NewEncoder(payload)
+					if err := encoder.Encode(lanhost); err != nil {
+						log.Error(err)
+						return
+					}
+					payloadString := strings.TrimSpace(fmt.Sprintf("%s", payload))
+					publish("mqtt-freebox/status/host/"+lanhost.ID, payloadString)
+
+				},
+			},
+		},
+	})
+
 	if err != nil {
-		return nil, false
-	}
-	for _, c := range calls {
-		if (c.Type == "missed") && (c.New) {
-			return &c, true
-		}
-	}
-	return nil, false
-}
-
-// notify by sms
-func notifySMS(msg string) {
-	if (SMS_LOGIN == "") || (SMS_PASS) == "" {
-		return
+		log.Fatal(err)
 	}
 
-	data := url.Values{
-		"user": {SMS_LOGIN},
-		"pass": {SMS_PASS},
-		"msg":  {msg}}
-	response, err := http.Get("https://smsapi.free-mobile.fr/sendmsg?" + data.Encode())
-	fmt.Printf("DEBUG:data=%v", data.Encode())
+	<-sigc
 
-	if err != nil {
-		log.Warn(response)
-	}
-
-	defer response.Body.Close()
-	body, err := ioutil.ReadAll(response.Body)
-
-	fmt.Printf("DEBUG:body=%v", body)
-	if err != nil {
-		log.Warn(response)
-	} else {
-		log.WithFields(log.Fields{"http status": response.Status}).Info("Seding SMS ...")
+	// Disconnect the Network Connection.
+	if err := cli.Disconnect(); err != nil {
+		panic(err)
 	}
 }
